1ce7cb72df9e13a401f6ebd45c9cb63a04a87548
Martin Orszulik <orszulik.m@outlook.com>
2019-01-25 15:40:36 +0100

    Finishing

---
+++ sw.js
@@ -0,0 +1,32 @@
+var cacheName = 'hello-world-page';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/galerie.html',
+  '/kontakt.html',
+  '/css/bulma.min.css',
+  '/css/bulma-carousel.css',
+  '/js/bulma-carousel.min.js'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
