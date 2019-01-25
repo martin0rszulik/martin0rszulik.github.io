9bff62deda8d8fe41ff5892292c202f9e8bb70e5
Martin Orszulik <orszulik.m@outlook.com>
2019-01-25 16:25:46 +0100

    Add homescreen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var cacheName = 'hello-world-page';
-var filesToCache = [
+var files = [
   '/',
   '/index.html',
   '/galerie.html',
@@ -9,24 +9,73 @@ var filesToCache = [
   '/js/bulma-carousel.min.js'
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+self.addEventListener('install', (event) => {
+    console.info('Event: Install');
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
+    event.waitUntil(
+      caches.open(cacheName)
+      .then((cache) => {
+        //[] of files to cache & if any of the file not present `addAll` will fail
+        return cache.addAll(files)
+        .then(() => {
+          console.info('All files are cached');
+          return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+        })
+        .catch((error) =>  {
+          console.error('Failed to cache', error);
+        })
+      })
+    );
+  });
 
-self.addEventListener('fetch', event => {
+
+  self.addEventListener('fetch', (event) => {
+  console.info('Event: Fetch');
+
+  var request = event.request;
+
+  //Tell the browser to wait for newtwork request and respond with below
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
+    //If request is already in cache, return it
+    caches.match(request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      //if request is not cached, add it to cache
+      return fetch(request).then((response) => {
+        var responseToCache = response.clone();
+        caches.open(cacheName).then((cache) => {
+            cache.put(request, responseToCache).catch((err) => {
+              console.warn(request.url + ': ' + err.message);
+            });
+          });
+
+        return response;
+      });
     })
   );
 });
+
+/*
+  ACTIVATE EVENT: triggered once after registering, also used to clean up caches.
+*/
+
+//Adding `activate` event listener
+self.addEventListener('activate', (event) => {
+  console.info('Event: Activate');
+
+  //Remove old and unwanted caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {     //cacheName = 'cache-v1'
+            return caches.delete(cache); //Deleting the cache
+          }
+        })
+      );
+    })
+  );
+});
+
